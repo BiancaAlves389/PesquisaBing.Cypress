15ff1c14b40b1d6632e494780bb259f677e02194
BiancaAlves389 <terceirabibi@gmail>
2026-05-06 21:48:30 -0300

    Ajusta teste Cypress e inclui screenshots

--- cypress/e2e/pesquisa.cy.js
+++ cypress/e2e/pesquisa.cy.js
@@ -6,13 +6,17 @@ describe('Pesquisa no Bing', () => {
 
     cy.visit('https://www.bing.com/?cc=br');
 
+    cy.get('body', { timeout: 10000 }).should('be.visible');
+
     cy.get('body').then(($body) => {
       if ($body.text().match(/aceitar|accept/i)) {
-        cy.contains(/aceitar|accept/i).click({ force: true });
+        cy.contains(/aceitar|accept/i, { timeout: 5000 })
+          .click({ force: true });
       }
     });
 
     cy.get('#sb_form_q')
+      .should('be.visible')
       .type('Livros JAVA{enter}');
 
     cy.get('#b_results', { timeout: 10000 })
