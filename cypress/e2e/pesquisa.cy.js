1d28bfc2a87c5791e40b212b7f59aead9f554120
BiancaAlves389 <terceirabibi@gmail>
2026-05-06 21:43:51 -0300

    fix: ajusta workflow e corrige teste Cypress

--- cypress/e2e/pesquisa.cy.js
+++ cypress/e2e/pesquisa.cy.js
@@ -1,26 +1,24 @@
 describe('Pesquisa no Bing', () => {
 
-    it('Fazer pesquisa', () => {
+  it('Fazer pesquisa', () => {
 
-        cy.on('uncaught:exception', () => false);
+    cy.on('uncaught:exception', () => false);
 
-        cy.visit('https://www.bing.com/?cc=br');
+    cy.visit('https://www.bing.com/?cc=br');
 
-        // aceita cookies se aparecer
-        cy.contains(/aceitar/i).click({ force: true });
-
-        cy.get('#sb_form_q')
-          .type('Livros JAVA');
-
-        // clicar no botão de busca (mais confiável que ENTER)
-        cy.get('#sb_form_go').click({ force: true });
-
-        // agora sim espera resultados
-        cy.get('#b_results', { timeout: 10000 })
-          .should('be.visible');
-
-        cy.screenshot();
-
+    cy.get('body').then(($body) => {
+      if ($body.text().match(/aceitar|accept/i)) {
+        cy.contains(/aceitar|accept/i).click({ force: true });
+      }
     });
+
+    cy.get('#sb_form_q')
+      .type('Livros JAVA{enter}');
+
+    cy.get('#b_results', { timeout: 10000 })
+      .should('be.visible');
+
+    cy.screenshot();
+  });
 
 });
